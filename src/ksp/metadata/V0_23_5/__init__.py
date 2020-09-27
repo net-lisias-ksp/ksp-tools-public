b9288ca7c2546961fcfb4451a309026ab631b6d0
Lisias <me@lisias.net>
2020-09-27 10:00:34 -0300

    Stubbing Module Metadata for every KSP I have

---
+++ src/ksp/metadata/V0_23_5/__init__.py
@@ -0,0 +1,21 @@
+#    This file is part of KSP Tools
+#    © 2020 LisiasT
+#
+#    KSP Tools is licensed as follows:
+#
+#        * GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+#
+#    KSP Tools is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#    You should have received a copy of the GNU General Public License 2.0
+#    along with KSP Tools, if not see <https://www.gnu.org/licenses/>.
+#
+
+import ksp.metadata.V0_23_5.Stock as Stock
+import ksp.metadata.V0_23_5.MakingHistory as MH
+import ksp.metadata.V0_23_5.Serenity as Serenity
+
+ALL_MODULES = Stock.MODULES | MH.MODULES | Serenity.MODULES
+
---
+++ src/ksp/metadata/__init__.py
@@ -0,0 +1,23 @@
+#    This file is part of KSP Tools
+#    © 2020 LisiasT
+#
+#    KSP Tools is licensed as follows:
+#
+#        * GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+#
+#    KSP Tools is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#    You should have received a copy of the GNU General Public License 2.0
+#    along with KSP Tools, if not see <https://www.gnu.org/licenses/>.
+#
+
+import ksp.metadata.V1_9_1
+
+def get(key):
+	key = "V" + key.replace(".", "_")
+
+	if "V1_9_1" == key: return ksp.metadata.V1_9_1
+
+	raise ModuleNotFoundError(key)
