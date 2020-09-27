646e3455c321ddba0719fc022933b012868fec47
Lisias <me@lisias.net>
2020-09-27 09:59:39 -0300

    Adding Module Metadata for KSP 1.9.1

---
+++ src/ksp/metadata/V1_9_1/MakingHistory.py
@@ -0,0 +1,24 @@
+#    This file is part of KSP Tools
+#    © 2020 LisiasT
+#
+#    KSP Tools is licensed as follows:
+#
+#        * GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+#
+#    KSP Tools is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#    You should have received a copy of the GNU General Public License 2.0
+#    along with KSP Tools, if not see <https://www.gnu.org/licenses/>.
+#
+'''
+Created on Sep 26, 2020
+
+@author: lisias
+'''
+
+MODULES = set([
+	"ModuleServiceModule",
+])
+
