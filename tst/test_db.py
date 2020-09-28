4b5d76b919fede06d462197f9450f4e0c028eca2
Lisias <me@lisias.net>
2020-09-27 23:53:29 -0300

    Testing the "database" with a simple sum of parts and modules by ksp version

---
+++ tst/test_db.py
@@ -0,0 +1,25 @@
+'''
+Created on Sep 27, 2020
+
+@author: lisias
+'''
+
+import ksp.metadata
+
+if __name__ == '__main__':
+	print ("Version \t ∑Modules \t ∑AllParts \t ∑Stock \t ∑MH \t ∑Serenity")
+
+	def s(x):
+		r = int(x.replace(".",""))
+		r = r / 10 if x.startswith("0") and x.count(".") > 1 else r
+		return r
+	for kspv in sorted(ksp.metadata.ALL_VERSIONS, key=s):
+		m = ksp.metadata.get(kspv)
+		print('{:s} \t\t {:d} \t\t {:d} \t\t {:d} \t\t {:d} \t\t {:d}'.format(
+				kspv,
+				len(m.ALL_MODULES),
+				len(m.ALL_PARTS),
+				len(m.Stock.PARTS),
+				len(m.MH.PARTS),
+				len(m.Serenity.PARTS)
+			))
